144026dae9e7e371bce6cb91c767039904274379
antleo1995 <antleo1995@gmail.com>
2017-04-27 11:17:39 -0400

    Recipe app - Deployment
    Modifies READ.me to include production URL and Repo. Removes / from
    end of prod URL to fix issue with Production URL - getting double //

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,7 +3,7 @@
 const config = {
   apiOrigins: {
     // production: 'https://ga-wdi-boston.herokuapp.com'
-    production: 'https://recipe-card-app.herokuapp.com/'
+    production: 'https://recipe-card-app.herokuapp.com'
   }
 }
 
